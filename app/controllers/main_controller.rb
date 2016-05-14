5bf1ba00611774e07e2fe4f2ce0507ed8e611d1a
Osmond Oscar <osmond.oranagwa@andela.com>
2016-05-14 20:00:11 +0100

    Add method for clearing and adding entries. Connect methods to keypad

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,16 +1,27 @@
 class MainController < UIViewController
   extend IB
 
-  attr_accessor
-
-
   ## ib outlets
   outlet :display, UILabel
+  outlet :entries, UILabel
 
 
   def clear_entry(sender)
-    puts "I wanna clear something"
+    label.text = ""
   end
 
+  def clear_last(sender)
+    @entries.pop
+    self.update_entries_display
+  end
 
+  def add_entry(sender)
+    @entries ||= []
+    @entries << sender.text
+    self.update_entries_display
+  end
+
+  def update_entries_display
+    entries.text = @entries.join(" ")
+  end
 end
