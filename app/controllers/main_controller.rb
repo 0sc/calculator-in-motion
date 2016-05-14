a7cca2250b76a0ee765615dbe44db735eb09fa13
Osmond Oscar <osmond.oranagwa@andela.com>
2016-05-14 23:36:50 +0100

    Implemennt basic arithmetic operations

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,24 +5,58 @@ class MainController < UIViewController
   outlet :display, UILabel
   outlet :entries, UILabel
 
+  def viewWillAppear(animated)
+    clear_entry
+  end
 
-  def clear_entry(sender)
-    @computation = []
-    self.update_entries_display
+  def clear_entry
+    @computation = ""
+    @result = 0
+    @values = ""
+    @operator_position = nil
+
+    update_entries_display
+    update_result_entry
   end
 
   def clear_last(sender)
-    @computation.pop
+    @computation = @computation[0...-1]
     self.update_entries_display
   end
 
   def add_entry(sender)
-    @computation ||= []
-    @computation << sender.titleLabel.text
+    @computation += sender.titleLabel.text
     self.update_entries_display
   end
 
   def update_entries_display
-    entries.text = @computation.join(" ")
+    entries.text = @computation
+  end
+
+  def add_operator(sender)
+    operator = sender.titleLabel.text
+    if @operator_position
+      if @computation.length == @operator_position
+        @computation = @computation[0...-1] + operator
+      else
+        evaluate
+        @computation = "#{@result}" + operator
+        @operator_position = @computation.length
+      end
+    else
+      @computation += operator
+      @operator_position = @computation.length
+    end
+    update_entries_display
+  end
+
+  def evaluate
+    op_pos = @operator_position - 1
+    @result = @computation[0...op_pos].to_i.send(@computation[op_pos], @computation[(op_pos + 1)..-1].to_i)
+    update_result_entry
+  end
+
+  def update_result_entry
+    display.text = @result.to_s
   end
 end
